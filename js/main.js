eb67bb8e4ccc21e890b29c0cd51441cd02725e97
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-07-12 16:36:44 +0200

    function for the bombs array created

--- js/main.js
+++ js/main.js
@@ -25,6 +25,9 @@ function onBtnClick() {
 
     // invoco la funzione che si occupera di aggiungere al DOM i vari quadrati
     printGrid(myContainer, gridList)
+
+    // array delle bombe per modalita`
+    const allBomb = arraybomb(squareCounts);
 }
 
 /**
@@ -84,4 +87,31 @@ function printGrid(container, squareList) {
     for (let i = 0; i < squareList.length; i++) {
         container.append(squareList[i]);
     }
+}
+
+// CONSEGNA 'JS-CAMPOMINATO-DOM'
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati -
+// abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti 
+// (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, 
+// cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+// devo creare un array di 16 numeri 
+/**
+ * 
+ * @param {number} squareCounts 
+ */
+function arraybomb(squareCounts) {
+    const numberBomb = [];
+
+    for (let i = 0; i < 16; i++) {
+        const casualNumber = Math.floor(Math.random() * squareCounts) + 1;
+        numberBomb.push(casualNumber);
+    }
+
+    console.log(numberBomb);
 }
